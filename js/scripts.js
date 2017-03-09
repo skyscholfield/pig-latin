bbcea3b1cd290a7f1d9bdb614b15943aebff0374
Oed Ronne and Sky Scholfield <sky@>
2017-03-08 19:33:11 -0800

    Add functionality to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,37 @@
-// take an input from the user
-  // split()
-  // reverse()
-// determine if a palendrome
-  //
-// return a yes or no answer
+//
+//
+//
+// function reverseArray(word){
+//   var separate = word.split("");
+//   var reverseArray = splitString.reverse();
+//   var joinArray = reverseArray.join("");
+//   return joinArray();
+//   }
+//
+// var palendrome = function(detection) {
+//   if (word === joinArray) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+//    var word = $("input#word").val();
+//    var result = palendrome(word);
+//  $(".word").text(year);
 
-
-
-function reverseArray(word){
-  var separate = word.split("");
-  var reverseArray = splitString.reverse();
-  var joinArray = reverseArray.join("");
-  return joinArray();
-  }
- // Our neighbors fixed the above by stringing together split, reverse, and join in one line
-var palendrome = function(detection) {
-  if (word === joinArray) {
-    return true;
-  } else {
-    return false;
-  }
-};
+var userInput = $("input#userInput").val();
+var result = userInput.split(" ");
 
 $(document).ready(function() {
   $("form#word-entry").submit(function(event) {
     event.preventDefault();
-    var word = $("input#word").val();
-    var result = palendrome(word);
 
-    $(".word").text(year);
-    if (!result) {                 // same as writing if (result === false)
+  if (unserInput != ) {                 // same as writing if (result === false)
       $(".not").text("not");
     } else {
       $(".not").text("");
     }
 
-    $("#result").text(result);
+    $(".pigOutput").text(result);
   });
 });
